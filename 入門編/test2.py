8b7b22c1fce209aa7d88632941e5fb094bcb3075
taka1taka2taka3 <38009331+taka1taka2taka3@users.noreply.github.com>
2018-05-22 23:03:45 +0900

    Merge branch 'master' of https://github.com/taka1taka2taka3/python_study

--- 入門編/test2.py
+++ 入門編/test2.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 print('モジュールのロード')
 
 def test():
@@ -8,3 +9,15 @@ if __name__ == '__main__':
     print('python-izm')
 #   print('パイソンイズム')
     test()
+=======
+print('モジュールのロード')
+
+def test():
+    print('関数：testを呼び出しました')
+ 
+if __name__ == '__main__':
+
+    print('python-izm')
+#   print('パイソンイズム')
+    test()
+>>>>>>> 83ed2accc33f91aed97d0b333ca89fd5c9a390c5
